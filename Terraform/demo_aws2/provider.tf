402fe8a7e23e022e165127ec152d32675371527b
Bart Thonissen <bart.thonissen@thomasmore.be>
2024-09-24 18:33:56 +0200

    aanpassen security

--- Terraform/demo_aws2/provider.tf
+++ Terraform/demo_aws2/provider.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "ASIAQMPFEMXF4DIJGHGD"
-  secret_key = "uMqSxHnMlOQbPGtgcb74Jjz3eOHBMICn0w1w9Gd1"
-  token="IQoJb3JpZ2luX2VjEJ3//////////wEaCXVzLXdlc3QtMiJHMEUCIQDYJs10CjPpw2DtDM/pfjWu159Cq42ia0BZ7AcBk8ZCOQIgYRW4qMsVzTn65UME0psJSmkZmGad4GnnCeuf0EsOagcqsQII1v//////////ARABGgwwMjY3ODcyMTA2OTkiDFL0TCJpEFf4hBnc7SqFAmdIUkVAPh5daPnyYMwBf0bKdV24QbRw7YUl3xyGW5Ad8cpnDuvArRYk9q2HVaP0ZMR5eRI1XTCuW4RMq1Km0Q5CW/AlmvuO7HPoAwQaiEkPnEBGMc8txaZgNTzJlA21nBXoTYO/nwZJgPANow3dQbwSySxgdPtOTlWg4SrxmBzFRvpZJhllhOEjtAPsqqaBSvFFfNdGBkouAwLyKbohGQplbX+2oh/h9rq80lSnGMdb8KL9jOlPG9wx3PfcQ4xJYT79VjMAJ4KXlsj9EsQlZL6jfwZ+mXHdZNxJPYEyLsxo2H43Ab7uKV2v7l4zfCyWr9OaJGtOQsePgx6BMLhxQLf3DMhwiTCm5cq3BjqdAa11mKHRiykIvOqb45ELsTyUTQu96j8lJT5CeKGZ2MzHggZAJMlqVpC10DJSaiKvgipcHL38r1e7VEyf6whOZFi+r9zJ84qVNnPA+iKIs/ZKM/5awqp8/mKaJUzLlvJwglw6JhTvLTkMKlB3GNFhgt9/0IjllPs0KQ33xuePjP84Paprvi9XbCEnSnA1/4rNQ7PBvG/DLmxsCn88VOM="
+  access_key = ""
+  secret_key = ""
+  token=""
   region = "us-east-1"
 }
