b5b9d70690f860d387fe52a3527845570f5bf0e3
BartTho <61376948+BartTho@users.noreply.github.com>
2024-09-27 11:32:19 +0200

    Update installatie.sh

--- Ansible/installatie.sh
+++ Ansible/installatie.sh
@@ -1,4 +1,5 @@
 sudo apt update && sudo apt upgrade -y
 udo apt install software-properties-common
-sudo add-apt-repository --ja --update ppa:ansible/ansible
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt update
 sudo apt install ansible
