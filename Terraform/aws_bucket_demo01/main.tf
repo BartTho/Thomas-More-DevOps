b05846688546b6fa9f4c5a43714439ad27d6bcdd
BartTho <61376948+BartTho@users.noreply.github.com>
2024-10-25 14:13:30 +0200

    Update main.tf

--- Terraform/aws_bucket_demo01/main.tf
+++ Terraform/aws_bucket_demo01/main.tf
@@ -2,36 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "mijn-tf-test-bucket"
-  tags = {
-    Name        = "Mijn Box"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "demo"{
+    bucket = "mijn-tf-bucket"
+
+    tags = {
+       Name         = "Mijn Bucket"
+       Environment  = "Dev"
+    }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.b.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
+resource "aws_s3_bucket_ownership_controls" "control"{
+    bucket = aws_s3_bucket.demo.id
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }    
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket_public_access_block" "control"{
+    bucket = aws_s3_bucket.demo.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
-
-  bucket = aws_s3_bucket.b.id
-  acl    = "public-read"
+resource "aws_s3_bucket_acl" "mijn_acl"{
+    depends_on = [
+      aws_s3_bucket_ownership_controls.control,
+      aws_s3_bucket_public_access_block.control
+    ]  
+    bucket = aws_s3_bucket.demo.id
+    acl = "public-read"  
 }
